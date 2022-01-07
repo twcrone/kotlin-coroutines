5c6e5016164e68c0cce200dbadaca3acb5210911
Todd W Crone <twcrone@gmail.com>
2022-01-07 11:11:49 -0500

    MOre flows

--- src/main/kotlin/CreatingFlows.kt
+++ src/main/kotlin/CreatingFlows.kt
@@ -1,13 +1,27 @@
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
 
 fun main() {
     runBlocking {
-        sendNumbers().collect {
-            println("Received $it")
+        val numbersFlow = sendNumbers()
+        println("Flow hasn't started yet")
+        println("Starting flow...")
+        withTimeoutOrNull(1000) {
+            numbersFlow.collect {
+                println("Received $it")
+            }
         }
     }
 }
 
-fun sendNumbers() = flowOf("one", "two", "three")
+fun sendNumbers() = flow {
+    listOf(1, 2, 3, 4).forEach {
+        delay(400)
+        emit(it)
+    }
+}
