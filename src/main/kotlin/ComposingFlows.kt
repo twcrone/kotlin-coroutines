c75f0c5874022dbd89af178bcd6ce5b55881a372
Todd W Crone <twcrone@gmail.com>
2022-01-07 11:28:55 -0500

    Composing Flows with zip() and combine()

---
+++ src/main/kotlin/ComposingFlows.kt
@@ -0,0 +1,31 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+    //    zip()
+        combine()
+    }
+}
+
+suspend fun combine() {
+    val numbers = (1..5).asFlow().onEach { delay(300) }
+    val values = flowOf("One", "Two", "Three", "Four", "Five")
+        .onEach { delay(400) }
+
+    numbers.combine(values) {a, b ->
+        "$a -> $b"
+    }
+        .collect { println(it) }
+}
+
+suspend fun zip() {
+    val english = flowOf("One", "Two")
+    val spanish = flowOf("Uno", "Dos")
+
+    english.zip(spanish) {e, s -> "$e -> $s" }
+        .collect {
+            println(it)
+        }
+}
